122b3c8a704b9babc31045eec9e40a2417260005
Michael Comella <michael.l.comella@gmail.com>
2012-09-14 00:42:31 -0400

    Made fonts init script handle existing files.

--- fonts/init.sh
+++ fonts/init.sh
@@ -11,8 +11,13 @@ cd ~/.fonts
 # Inconsolata-dz
 # TODO: Make this use a loop if I ever get more fonts.
 # TODO: Check if font is installed system-wide before installing per user.
-wget http://media.nodnod.net/Inconsolata-dz.otf.zip -o ~/dotfiles_font
+if [ ! -f Inconsolata-dz.otf ];
+then
+  echo -e "\tDownloading Inconsolata-dz..."
+  wget http://media.nodnod.net/Inconsolata-dz.otf.zip -a ~/dotfiles_font
+else
+  echo -e "\tInconsolata-dz already exists. Skipping..."
+fi
 
-# Cleanup.
-unzip *.zip > /dev/null
-rm -f *.zip
+unzip *.zip &>> ~/dotfiles_font && \
+    rm -f *.zip # Cleanup
