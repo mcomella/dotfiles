40f68e20ce0d18ca29a07d07a871f8209ab57a71
Michael Comella <michael.l.comella@gmail.com>
2012-09-14 00:22:57 -0400

    Added fonts.

---
+++ fonts/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. ./func.sh
+
+echo "Setting up font configuration:";
+
+# Create directories.
+mkdir -p ~/.fonts
+cd ~/.fonts
+
+# Inconsolata-dz
+# TODO: Make this use a loop if I ever get more fonts.
+# TODO: Check if font is installed system-wide before installing per user.
+wget http://media.nodnod.net/Inconsolata-dz.otf.zip -o ~/font_output
+
+# Cleanup.
+unzip *.zip > /dev/null
+rm -f *.zip
