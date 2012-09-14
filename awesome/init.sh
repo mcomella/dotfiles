22d0185298dc2c3abbd6d65889d025ded333772d
Michael Comella <michael.l.comella@gmail.com>
2012-09-14 00:10:25 -0400

    Fixed awesome config setup.

--- awesome/init.sh
+++ awesome/init.sh
@@ -4,5 +4,6 @@
 
 echo "Setting up awesome configuration:";
 
+mkdir -p ~/.config/awesome
 linkOrWarn awesome/rc.lua ~/.config/awesome/rc.lua
 linkOrWarn awesome/theme.lua ~/.config/awesome/theme.lua
