379fad7a7df49d34e528a9e669c255f9ba1d0272
Michael Comella <michael.l.comella@gmail.com>
2012-09-14 00:25:42 -0400

    Changed wget output filename again because I'm indecisive.

--- fonts/init.sh
+++ fonts/init.sh
@@ -11,7 +11,7 @@ cd ~/.fonts
 # Inconsolata-dz
 # TODO: Make this use a loop if I ever get more fonts.
 # TODO: Check if font is installed system-wide before installing per user.
-wget http://media.nodnod.net/Inconsolata-dz.otf.zip -o ~/dotfiles_font_output
+wget http://media.nodnod.net/Inconsolata-dz.otf.zip -o ~/dotfiles_font
 
 # Cleanup.
 unzip *.zip > /dev/null
