d060d20330308e7e5d4b79898428ce59e1ae6694
Michael Comella <michael.l.comella@gmail.com>
2012-10-12 11:52:36 -0400

    Modify font init file.
    Fixed indentation and change the output logfile on wget.

--- fonts/init.sh
+++ fonts/init.sh
@@ -11,12 +11,12 @@ cd ~/.fonts
 # Inconsolata-dz
 # TODO: Make this use a loop if I ever get more fonts.
 # TODO: Check if font is installed system-wide before installing per user.
-if [ ! -f Inconsolata-dz.otf ];
-then
-  echo -e "\tDownloading Inconsolata-dz..."
-  wget http://media.nodnod.net/Inconsolata-dz.otf.zip -a ~/dotfiles_font
+if [ ! -f Inconsolata-dz.otf ]; then
+    echo -e "\tDownloading Inconsolata-dz..."
+    wget http://media.nodnod.net/Inconsolata-dz.otf.zip -a ~/.dotfiles/log
+# TODO: Unzip?
 else
-  echo -e "\tInconsolata-dz already exists. Skipping..."
+    echo -e "\tInconsolata-dz already exists. Skipping..."
 fi
 
 unzip *.zip &>> ~/dotfiles_font && \
