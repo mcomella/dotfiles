65a3564b7597d67cbbad6ce4c3e1f48dd8d800e4
Michael Comella <michael.l.comella@gmail.com>
2012-11-14 23:57:16 -0500

    Add custom layout icons.
    This involved adding some path variables to theme.rc.

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -1,3 +1,5 @@
+config_dir = "/home/mcomella/.config/awesome/"
+
 -- Solarized colors --
 base03  = "#002b36"
 base02  = "#073642"
@@ -70,7 +72,27 @@ theme.menu_width  = "100"
 -- beautiful.variable in your rc.lua
 --theme.bg_widget = "#cc0000"
 
--- Define the image to load
+-- Layout icons --
+layout_dir = config_dir .. "themes/gray/layouts/"
+theme.layout_fairh =        layout_dir .. "fairhw.png"
+theme.layout_fairv =        layout_dir .. "fairvw.png"
+theme.layout_floating  =    layout_dir .. "floatingw.png"
+theme.layout_magnifier =    layout_dir .. "magnifierw.png"
+theme.layout_max =          layout_dir .. "maxw.png"
+theme.layout_fullscreen =   layout_dir .. "fullscreenw.png"
+theme.layout_tilebottom =   layout_dir .. "tilebottomw.png"
+theme.layout_tileleft   =   layout_dir .. "tileleftw.png"
+theme.layout_tile =         layout_dir .. "tilew.png"
+theme.layout_tiletop =      layout_dir .. "tiletopw.png"
+theme.layout_spiral  =      layout_dir .. "spiralw.png"
+theme.layout_dwindle =      layout_dir .. "dwindlew.png"
+
+theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
+
+-- Wallpaper --
+theme.wallpaper_cmd = { "feh --bg-scale '$HOME/.config/awesome/themes/bg/bg.jpg' '$HOME/.config/awesome/themes/bg/bg_tall.jpg'" }
+
+-- Titlebar icons --
 theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
 theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
 
@@ -93,26 +115,6 @@ theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
-
--- You can use your own command to set your wallpaper
--- theme.wallpaper_cmd = { "awsetbg -f /home/mcomella/.config/awesome/background.jpg" }
-theme.wallpaper_cmd = { "feh --bg-scale '$HOME/.config/awesome/themes/bg.jpg' '$HOME/.config/awesome/themes/bg_tall.jpg'" }
-
--- You can use your own layout icons like this:
-theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
-theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
-theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floatingw.png"
-theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
-theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
-theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
-theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
-theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
-theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
-theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
-
-theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
 return theme
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80
