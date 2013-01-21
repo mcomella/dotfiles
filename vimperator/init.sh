92cf78910acc3cd88f3fce8706c5ad5399682974
Michael Comella <michael.l.comella@gmail.com>
2013-01-21 11:51:37 -0500

    vimperator: Add large colorscheme; various opts.

--- vimperator/init.sh
+++ vimperator/init.sh
@@ -5,3 +5,6 @@
 echo "Setting up vimperator configuration:"
 
 linkOrWarn vimperator/vimperatorrc ~/.vimperatorrc
+mkdir -p ~/.vimperator/colors/
+linkOrWarn vimperator/colors/large.vimp ~/.vimperator/colors/large.vimp
+linkOrWarn vimperator/colors/solarized.vimp ~/.vimperator/colors/solarized.vimp
