2ea7b9a45b126393a670a7fb99ab78a10c5b78e1
Michael Comella <michael.l.comella@gmail.com>
2013-01-21 12:41:50 -0500

    vimperator: colors large->solarized.

--- vimperator/init.sh
+++ vimperator/init.sh
@@ -6,4 +6,4 @@ echo "Setting up vimperator configuration:"
 
 linkOrWarn vimperator/vimperatorrc ~/.vimperatorrc
 mkdir -p ~/.vimperator/colors/
-linkOrWarn vimperator/colors/large.vimp ~/.vimperator/colors/large.vimp
+linkOrWarn vimperator/colors/solarized.vimp ~/.vimperator/colors/solarized.vimp
