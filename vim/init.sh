6163e333405f7a1cc1c720c1ceefe8637b47deea
Michael Comella <michael.l.comella@gmail.com>
2013-04-18 20:48:59 -0400

    vim: Add vim-sleuth; remove indent config.

--- vim/init.sh
+++ vim/init.sh
@@ -26,3 +26,4 @@ gitCloneOrWarn vim-coffee-script git://github.com/kchmck/vim-coffee-script.git \
         HEAD ~/.vim/bundle/vim-coffee-script
 gitCloneOrWarn ctrlp https://github.com/kien/ctrlp.vim.git HEAD ~/.vim/bundle/ctrlp
 gitCloneOrWarn closetag https://github.com/vim-scripts/closetag.vim HEAD ~/.vim/scripts/closetag.vim
+gitCloneOrWarn vim-sleuth https://github.com/tpope/vim-sleuth HEAD ~/.vim/bundle/vim-sleuth
