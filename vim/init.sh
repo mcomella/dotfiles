ea7f557b2a92ffe908888bcbed18bae0b42e2563
Michael Comella <michael.l.comella@gmail.com>
2016-03-31 08:43:07 -0700

    vim: add vundle init.

--- vim/init.sh
+++ vim/init.sh
@@ -10,21 +10,9 @@ linkOrWarn vim/vimrc ~/.vimrc
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 mkdir -p ~/.vim/scripts
 
-# Pathogen
+# Vundle. TODO: May not actually work.
 if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
-    echo -e "\tcurl pathogen..."
-    curl -Sso ~/.vim/autoload/pathogen.vim \
-            https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+    gitCloneOrWarn Vundle.vim git://github.com/VundleVim/Vundle.vim.git \
+        HEAD ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
 fi
-
-gitCloneOrWarn indent-guides git://github.com/nathanaelkane/vim-indent-guides.git \
-        HEAD ~/.vim/bundle/vim-indent-guides
-gitCloneOrWarn solarized git://github.com/altercation/vim-colors-solarized.git \
-        HEAD ~/.vim/bundle/vim-colors-solarized
-gitCloneOrWarn tagbar git://github.com/majutsushi/tagbar.git HEAD ~/.vim/bundle/tagbar
-gitCloneOrWarn vim-coffee-script git://github.com/kchmck/vim-coffee-script.git \
-        HEAD ~/.vim/bundle/vim-coffee-script
-gitCloneOrWarn ctrlp https://github.com/kien/ctrlp.vim.git HEAD ~/.vim/bundle/ctrlp
-gitCloneOrWarn closetag https://github.com/vim-scripts/closetag.vim HEAD ~/.vim/scripts/closetag.vim
-gitCloneOrWarn vim-less https://github.com/groenewege/vim-less HEAD ~/.vim/bundle/vim-less
-gitCloneOrWarn vim-sleuth https://github.com/tpope/vim-sleuth HEAD ~/.vim/bundle/vim-sleuth
