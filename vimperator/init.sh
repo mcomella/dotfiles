f88787519e31d199005b95c518aa69981961c25d
Michael Comella <michael.l.comella@gmail.com>
2017-01-19 16:21:01 -0800

    vimperator: rm.

--- vimperator/init.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-. ./func.sh
-
-echo "Setting up vimperator configuration:"
-
-linkOrWarn vimperator/vimperatorrc ~/.vimperatorrc
-mkdir -p ~/.vimperator/colors/
-linkOrWarn vimperator/colors/solarized.vimp ~/.vimperator/colors/solarized.vimp
