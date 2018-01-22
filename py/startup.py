4dc98ffcf03ce8ed65cb2d872909156e4c43a725
Michael Comella <michael.l.comella@gmail.com>
2018-01-22 15:31:13 -0800

    mass update

--- py/startup.py
+++ py/startup.py
@@ -1,2 +1,7 @@
-# via https://mail.python.org/pipermail/tutor/2012-August/090729.html
+# startup via https://mail.python.org/pipermail/tutor/2012-August/090729.html
 from pprint import pprint as pp
+import json
+
+import sys
+if sys.version.startswith('3'):  # Python 3
+    from importlib import reload
